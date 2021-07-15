a5c9119607c52af8a1a49f1bcb8d8e2f524c8630
walid <walid@dr-2.eu>
2021-07-15 01:15:31 +0100

    mapper photourl

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -22,7 +22,8 @@ namespace API.Helpers
             CreateMap<Post, PostDto>()
                 .ForMember(dest => dest.Likes, opt => opt.MapFrom(src =>src.Postlikes.Count()))
                 .ForMember(dest => dest.posterName, opt => opt.MapFrom(src => src.Poster.KnownAs))
-                .ForMember(dest => dest.PosterPhotoUrl, opt => opt.MapFrom(src => src.Poster.Photos.FirstOrDefault().Url));
+                .ForMember(dest => dest.PosterPhotoUrl, opt => opt.MapFrom(src => src.Poster.Photos.FirstOrDefault().Url))
+                .ForMember(dest => dest.PhotoUrl, opt => opt.MapFrom(src => src.PhotoUrl));
             CreateMap<Message, MessageDto>()
                 .ForMember(dest => dest.SenderPhotoUrl, opt => opt.MapFrom(src => 
                     src.Sender.Photos.FirstOrDefault(x => x.IsMain).Url))
