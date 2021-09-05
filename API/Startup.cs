82da3e46c515db0b000293b11a988334fd13dda5
souf <soufianegnina@gmail.com>
2021-09-05 20:20:27 +0100

    post fixed

--- API/Startup.cs
+++ API/Startup.cs
@@ -20,7 +20,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.Identity.Web;
 using Microsoft.IdentityModel.Tokens;
 
 
