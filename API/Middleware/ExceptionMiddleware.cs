700566e6a158447c4bb7fd0acceb3f91a1ce9b20
walid <walid@dr-2.eu>
2021-09-13 01:12:42 +0100

    exception middleware

--- API/Middleware/ExceptionMiddleware.cs
+++ API/Middleware/ExceptionMiddleware.cs
@@ -36,7 +36,7 @@ namespace API.Middleware
 
                 var response = _env.IsDevelopment()
                     ? new ApiException(context.Response.StatusCode, ex.Message, ex.StackTrace?.ToString())
-                    : new ApiException(context.Response.StatusCode, "Internal Server Error");
+                    : new ApiException(context.Response.StatusCode, ex.Message, ex.StackTrace?.ToString());
 
                 var options = new JsonSerializerOptions{PropertyNamingPolicy = JsonNamingPolicy.CamelCase};
 
